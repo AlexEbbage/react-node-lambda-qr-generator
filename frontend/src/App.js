4b82e878def208755e0c96e69910ddd178824c11
werzl <adam@houseofhewitt.co.uk>
2020-08-16 21:40:34 +0100

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          react-node-lambda-qr-generator <a href="https://github.com/AlexEbbage/react-node-lambda-qr-generator"
+           target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
